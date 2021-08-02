68ad42977388c95d55118bd7e732a300f924f801
Danilin Aleksandr <Fodster@rambler.ru>
2021-08-02 12:34:10 +0300

    Стартовое окно

---
+++ mdViewer/StartWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+
+namespace mdViewer {
+    public partial class StartWindow : Window
+    {
+        public StartWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BuildDependensies(object sender, RoutedEventArgs e)
+        {
+            IVisual visual = new MainWindow();
+            Core core = new Core(visual);
+
+            Hide();
+            core.Start();
+
+            Close();
+        }
+    }
+}
