23f6fe331199973c0ce332908d4e8856d8443a2e
Danilin Aleksandr <Fodster@rambler.ru>
2021-08-02 13:51:54 +0300

    Namespace fix

--- mdViewer/Properties/Resources.Designer.cs
+++ mdViewer/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WPFUserInterface.Properties
+namespace mdViewer.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace WPFUserInterface.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WPFUserInterface.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("mdViewer.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- mdViewer/Properties/Settings.Designer.cs
+++ mdViewer/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WPFUserInterface.Properties
+namespace mdViewer.Properties
 {
 
 
